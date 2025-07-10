36b204bf2b7087b042fa67251eafc8877f0b30ef
Diego Maldonado <71333756+Diegosuwu@users.noreply.github.com>
2025-07-10 10:54:26 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,9 @@
+const { Events } = require('discord.js');
 
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		console.log(`logged in ${client.user.tag}`);
+	},
+};
